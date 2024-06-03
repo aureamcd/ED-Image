e0d06d067fc9cc4197c6869c5506f34bca70e979
aureamcd <157396271+aureamcd@users.noreply.github.com>
2024-06-03 16:41:49 -0300

    Update main.c
    m

--- main.c
+++ main.c
@@ -2,19 +2,16 @@
 #include "image.h"
 
 int main() {
-    
-    ImageGray *image = create_image_gray();  
+    ImageGray *image = create_image_gray();
     if (!image) {
         fprintf(stderr, "Erro ao criar a imagem\n");
         return 1;
     }
 
-
     printf("Dimensões da imagem: %d x %d\n", image->dim.largura, image->dim.altura);
     printf("Valor do primeiro pixel: %d\n", image->pixels[0].value);
 
-    
     free_image_gray(image);
 
-    return 0;dd
+    return 0;
 }
