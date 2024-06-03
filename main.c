e023df937ca3c865b55c78aec2ca80b91cd5c599
CR4ZY_DI4M0ND <luis.ramos@ufpi.edu.br>
2024-06-03 18:10:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,27 @@
 #include "image.h"
 
 int main() {
-    ImageGray *image = create_image_gray();
-    if (!image) {
+    ImageGray *imagecinza = create_image_gray();
+    if (!imagecinza) {
         fprintf(stderr, "Erro ao criar a imagem\n");
         return 1;
     }
 
-    printf("Dimensões da imagem: %d x %d\n", image->dim.largura, image->dim.altura);
-    printf("Valor do primeiro pixel: %d\n", image->pixels[0].value);
+    printf("Dimensões da imagem: %d x %d\n", imagecinza->dim.largura, imagecinza->dim.altura);
+    printf("Valor do primeiro pixel: %d\n", imagecinza->pixels[0].value);
 
-    free_image_gray(image);
+    free_image_gray(imagecinza);
+
+    ImageRGB *imagergb = create_image_rgb();
+    if (!imagergb) {
+        fprintf(stderr, "Erro ao criar a imagem\n");
+        return 1;
+    }
+
+    printf("Dimensões da imagem: %d x %d\n", imagergb->dim.largura, imagergb->dim.altura);
+    printf("Valor do primeiro pixel: %d\n", imagergb->pixels[0].red,imagergb->pixels[0].green,imagergb->pixels[0].blue);
+
+    free_image_gray(imagergb);
 
     return 0;
 }
