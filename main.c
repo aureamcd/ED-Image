37dc4467255e80ec851a7fe5df72996c8c31cd1c
CR4ZY_DI4M0ND <luis.ramos@ufpi.edu.br>
2024-06-03 16:12:11 -0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "image.h"
+
+int main() {
+    
+    ImageGray *image = create_image_gray();
+    if (!image) {
+        fprintf(stderr, "Erro ao criar a imagem\n");
+        return 1;
+    }
+
+
+    printf("Dimensões da imagem: %d x %d\n", image->dim.largura, image->dim.altura);
+    printf("Valor do primeiro pixel: %d\n", image->pixels[0].value);
+
+    
+    free_image_gray(image);
+
+    return 0;
+}
