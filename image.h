eac6e22cca97c7b8e8b3296e554737d69b6888c3
aureamcd <157396271+aureamcd@users.noreply.github.com>
2024-06-29 20:05:59 -0300

    Update image.h

--- image.h
+++ image.h
@@ -1,47 +1,65 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
-typedef struct dimensoes {
+typedef struct dimensoes
+{
     int altura, largura;
 } Dimensoes;
 
-typedef struct pixelRGB {
+typedef struct pixelRGB
+{
     int red, blue, green;
 } PixelRGB;
 
-typedef struct pixelGray {
+typedef struct pixelGray
+{
     int value;
 } PixelGray;
 
-typedef struct imageGray {
+typedef struct imageGray
+{
     Dimensoes dim;
     PixelGray *pixels;
 } ImageGray;
 
-typedef struct imageRGB {
+typedef struct imageRGB
+{
     Dimensoes dim;
     PixelRGB *pixels;
 } ImageRGB;
 
-typedef struct elemento {
+typedef struct elemento
+{
     char filename[50];
     char alt[50];
     struct elemento *prox;
     struct elemento *ant;
 } Elemento;
 
-typedef struct lista {
+typedef struct lista
+{
     Elemento *inicio;
     Elemento *fim;
-    int tam;    
+    int tam;
 } Lista;
 
+void print_menu();
+void print_alter();
+void print_hist();
+
+void swit_hist(Lista *lista);
+void swit_gray(Lista *lista, ImageGray *image_gray, char *filename);
+void swit_rgb(Lista *lista, ImageRGB *image_rgb, char *filename);
+
+void call_python_script(char *py, char *funcao, char *txt, char *output);
 
 Lista *criaLista();
 void adicionar_no_lista(Lista *lista, char *filename);
 void liberar_lista(Lista *lista);
 void percorrer_lista(Lista *lista);
+
 void imprimir_historico(Lista *lista);
+void add_ed(Lista *lista);
 
 // Funções de leitura de arquivo
 ImageGray *read_image_gray(char *filename);
@@ -66,10 +84,10 @@ void transpose_RGB(ImageRGB *image, Lista *lista, char *filename_rgb);
 
 // Manipulação por pixel para ImageGray
 void clahe_gray(ImageGray *image, Lista *lista, char *filename_gray);
-void median_blur_gray(ImageGray *image, Lista* lista, char *filename_rgb);
+void median_blur_gray(ImageGray *image, Lista *lista, char *filename_rgb);
 
 // Manipulação por pixel para ImageRGB
 void clahe_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
-void median_blur_rgb(ImageRGB *image, Lista* lista, char *filename_rgb);
+void median_blur_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
 
 #endif // IMAGE_H
