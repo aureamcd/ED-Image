ba1e6b7488024b3443072dd6f31be0c0fb7963b7
aureamcd <157396271+aureamcd@users.noreply.github.com>
2024-07-07 13:42:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -15,12 +15,17 @@ int main()
     Lista *lista_rgb = criaLista();
     char nomen[100];
     char nomea[100];
+    lista_gray->cont = 0;
+    lista_rgb->cont = 0;
 
     do
     {
 
         do
         {
+            lista_gray->cont++;
+            lista_rgb->cont++;
+
             print_menu();
             printf("\nDigite sua escolha: ");
             scanf("%d", &op);
@@ -37,6 +42,7 @@ int main()
                 {
                     printf("\nDimensoes da imagem em escala de cinza: %d x %d\n", image_gray->dim.largura, image_gray->dim.altura);
                     printf("Valor do primeiro pixel em escala de cinza: %d\n", image_gray->pixels[0].value);
+
                     create_image_gray(image_gray, lista_gray, filename_gray);
 
                     swit_gray(lista_gray, image_gray, filename_gray);
@@ -78,10 +84,6 @@ int main()
                     printf("Valor do primeiro pixel: Red %d, Green %d, Blue %d\n", image_rgb->pixels[0].red, image_rgb->pixels[0].green, image_rgb->pixels[0].blue);
                     create_image_rgb(image_rgb, lista_rgb, filename_rgb);
 
-                    char altered_filename_rgb[100];
-                    snprintf(altered_filename_rgb, sizeof(altered_filename_rgb), "rgb.txt%d", lista_rgb->tam);
-                    call_python_script("image_utils.py", "image_rgb_from_txt", altered_filename_rgb, "atual_rgb.png");
-
                     swit_rgb(lista_rgb, image_rgb, filename_rgb);
 
                     // Após aplicar a edição
@@ -114,9 +116,35 @@ int main()
                 printf("Opção 3 selecionada - implementar leitura e processamento de imagem.\n");
                 break;
 
-            case 4:
-                liberar_lista(lista_gray);
-                liberar_lista(lista_rgb);
+            case 4:       
+            if (filename_gray[0] != '\0')
+    {
+        char base_name[100];
+        strcpy(base_name, filename_gray);
+        char *dot = strrchr(base_name, '.');
+        if (dot != NULL && strcmp(dot, ".txt") == 0)
+        {
+            *dot = '\0';
+        }
+        delete_temp_files(lista_gray, base_name);
+        delete_current_png(base_name); // Deletar PNG atual
+    }
+    if (filename_rgb[0] != '\0')
+    {
+        char base_name[100];
+        strcpy(base_name, filename_rgb);
+        char *dot = strrchr(base_name, '.');
+        if (dot != NULL && strcmp(dot, ".txt") == 0)
+        {
+            *dot = '\0';
+        }
+        delete_temp_files(lista_rgb, base_name);
+        delete_current_png(base_name); // Deletar PNG atual
+    }
+
+    liberar_lista(lista_gray);
+    liberar_lista(lista_rgb);      
+                
                 printf("Programa encerrado.\n");
                 return 0;
 
@@ -127,5 +155,33 @@ int main()
         } while (op != 4);
     } while (opc == 's');
 
-return 0;
+    if (filename_gray[0] != '\0')
+    {
+        char base_name[100];
+        strcpy(base_name, filename_gray);
+        char *dot = strrchr(base_name, '.');
+        if (dot != NULL && strcmp(dot, ".txt") == 0)
+        {
+            *dot = '\0';
+        }
+        delete_temp_files(lista_gray, base_name);
+        delete_current_png(base_name); // Deletar PNG atual
+    }
+    if (filename_rgb[0] != '\0')
+    {
+        char base_name[100];
+        strcpy(base_name, filename_rgb);
+        char *dot = strrchr(base_name, '.');
+        if (dot != NULL && strcmp(dot, ".txt") == 0)
+        {
+            *dot = '\0';
+        }
+        delete_temp_files(lista_rgb, base_name);
+        delete_current_png(base_name); // Deletar PNG atual
+    }
+
+    liberar_lista(lista_gray);
+    liberar_lista(lista_rgb);
+
+    return 0;
 }
