3239116678cd4298bae5f914912c9a0f3cb174e2
aureamcd <157396271+aureamcd@users.noreply.github.com>
2024-07-10 19:50:38 -0300

    Update image.h

--- image.h
+++ image.h
@@ -30,19 +30,10 @@ typedef struct imageRGB
     int tam;
 } ImageRGB;
 
-typedef struct pilha
-{
-    ImageGray *imagegray;
-    ImageRGB *imagergb;
-    struct pilha *prox;    
-    char alt[50];
-} Pilha;
-
 typedef struct elemento
 {
     char filename[50];
-    Pilha *pilha;
-    int tam_listap;
+    char ult_alt[50];   
     struct elemento *prox;
     struct elemento *ant;
 } Elemento;
@@ -50,40 +41,34 @@ typedef struct elemento
 typedef struct lista
 {
     Elemento *inicio;
-    Elemento *fim;
-    Elemento *ultimo_modificado;
-    int tam;
-    int cont;
+    Elemento *fim;    
+    int tam;    
 } Lista;
 
-
 void print_menu();
-void print_alter();
-void print_hist();
 
+void print_menug();
+void swit_menug(Lista *lista, ImageGray *image_gray, char *filename);
+void swit_menur(Lista *lista, ImageRGB *image_rgb, char *filename);
+
+void print_alter();
 void swit_gray(Lista *lista, ImageGray *image_gray, char *filename);
 void swit_rgb(Lista *lista, ImageRGB *image_rgb, char *filename);
 
+void print_hist();
 void swit_hist_gray(Lista *lista, ImageGray *image_gray);
 void swit_hist_rgb(Lista *lista, ImageRGB *image_rgb);
-
-void delete_temp_files(Lista *lista, const char *base_name);
-void delete_current_png(const char *base_name);
-
-void call_python_script(char *py, char *funcao, char *txt, char *output);
 
 Lista *criaLista();
 void adicionar_no_lista(Lista *lista, char *filename);
 void liberar_lista(Lista *lista);
-void percorrer_lista(Lista *lista);
-
-void push_pilha_gray(Lista *lista, ImageGray *image_gray, char *filename, char *alteracao);
-Pilha *pop_pilha(Lista *lista);
-void desfazer_ultima_alteracaogray(Lista *lista, ImageGray *image_gray);
-void desfazer_ultima_alteracaorgb(Lista *lista, ImageRGB *image_rgb);
+void remover_ultimo_lista(Lista *lista);
+void buscar_e_gerar_imagem(Lista *lista);
+void deletar_renomear_txt(Lista *lista);
 
 
 void imprimir_historico(Lista *lista);
+
 void added_gray(Lista *lista, ImageGray *image_gray);
 void added_rgb(Lista *lista, ImageRGB *image_rgb);
 
@@ -91,42 +76,36 @@ void added_rgb(Lista *lista, ImageRGB *image_rgb);
 ImageGray *read_image_gray(char *filename);
 ImageRGB *read_image_rgb(char *filename);
 
-// Funções de criação e liberação
+// Funções de criação
 void create_image_gray(ImageGray *image, Lista *lista, char *filename_gray);
-void free_image_gray(ImageGray *image);
-
 void create_image_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
-void free_image_rgb(ImageRGB *image);
 
 // Operações para ImageGray
 void flip_vertical_gray(ImageGray *image, Lista *lista, char *filename_gray);
 void flip_horizontal_gray(ImageGray *image, Lista *lista, char *filename_gray);
 void transpose_gray(ImageGray *image, Lista *lista, char *filename_gray);
+void clahe_gray(ImageGray *image, Lista *lista, char *filename_gray);
+void median_blur_gray(ImageGray *image, Lista *lista, char *filename_rgb);
+void adicionar_aleatorio_gray(ImageGray *image, Lista *lista, char *filename_gray);
+void desfazer_alteracaogray(Lista *lista);
+
 
 // Operações para ImageRGB
 void flip_vertical_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
 void flip_horizontal_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
 void transpose_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
-
-// Manipulação por pixel para ImageGray
-void clahe_gray(ImageGray *image, Lista *lista, char *filename_gray);
-void median_blur_gray(ImageGray *image, Lista *lista, char *filename_rgb);
-
-// Manipulação por pixel para ImageRGB
 void clahe_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
 void median_blur_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
-
-void remover_ultimo_lista(Lista *lista);
-void desfazer_alteracaogray(Lista *lista);
-
-void desfazer_alteracaorgb(Lista *lista);
-
-void adicionar_aleatorio_gray(ImageGray *image, Lista *lista, char *filename_gray);
 void adicionar_aleatorio_rgb(ImageRGB *image, Lista *lista, char *filename_rgb);
-
-void desfazer_alteracaogray(Lista *lista);
 void desfazer_alteracaorgb(Lista *lista);
 
+
+void delete_temp_files(Lista *lista, const char *base_name);
+void delete_current_png(const char *base_name);
+
+void call_python_script(char *py, char *funcao, char *txt, char *output);
+int compararg(const void *a, const void *b);
+int comparar(const void *a, const void *b);
 
 
 #endif // IMAGE_H
